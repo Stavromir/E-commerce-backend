b68b75992b2cff45abb80848c9e492ca4d0f54f4
Stavromir Kasapov <127794574+Stavromir@users.noreply.github.com>
2024-03-21 14:39:06 +0200

    Initial project commit

---
+++ src/main/java/bg/softuni/Spring/ecommerce/app/SpringECommerceAppApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.Spring.ecommerce.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringECommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringECommerceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/Spring/ecommerce/app/SpringECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.Spring.ecommerce.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
