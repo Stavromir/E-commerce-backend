15cce2ec32b49902b1d1afd123604bf4ed2fff83
Stavromir Kasapov <127794574+Stavromir@users.noreply.github.com>
2024-04-24 18:03:12 +0300

    Implement UnitTest for CartItemService

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/CartItemServiceImplTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/CartItemServiceImplTest.java
@@ -1,42 +1,78 @@
 package bg.softuni.Spring.ecommerce.app.service.impl;
 
+import bg.softuni.Spring.ecommerce.app.model.dto.AddProductInCartDto;
 import bg.softuni.Spring.ecommerce.app.model.entity.CartItemEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.OrderEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.UserEntity;
 import bg.softuni.Spring.ecommerce.app.repository.CartItemRepository;
 import bg.softuni.Spring.ecommerce.app.service.CartItemService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CartItemServiceImplTest {
 
+    public static final Long PRODUCT_ID = 1L;
+    public static final Long ORDER_ID = 2L;
+    public static final Long USER_ID = 3L;
+
+
     private CartItemService cartItemServiceToTest;
 
     @Mock
     private CartItemRepository cartItemRepository;
 
+    @BeforeEach
+    void setUp() {
+        this.cartItemServiceToTest = new CartItemServiceImpl(cartItemRepository);
+    }
 
+
+    @Test
     void testFindCartItemByProductIdOrderIdAndUserId() {
+        //Arrange
         CartItemEntity testCartItem = createTestCartItem();
 
-        Mockito.when(cartItemRepository.findByUserIdAndProductIdAndOrderId(3L,1L, 2L));
+        Mockito.when(cartItemRepository.findByUserIdAndProductIdAndOrderId(USER_ID, PRODUCT_ID, ORDER_ID))
+                .thenReturn(Optional.of(testCartItem));
 
+        //Act
+        AddProductInCartDto productInCartDto = createProductInCardDto();
+        CartItemEntity cartItem = cartItemServiceToTest
+                .findByProductIdAndOrderIdAndUserID(productInCartDto, testCartItem.getOrder().getId());
 
+        //Assert
+
+        assertNotNull(cartItem);
+
+        assertEquals(testCartItem.getProduct().getId(), cartItem.getProduct().getId());
+        assertEquals(testCartItem.getOrder().getId(), cartItem.getOrder().getId());
+        assertEquals(testCartItem.getUser().getId(), cartItem.getUser().getId());
+    }
+
+    private AddProductInCartDto createProductInCardDto() {
+        return new AddProductInCartDto()
+                .setUserId(USER_ID)
+                .setProductId(PRODUCT_ID);
     }
 
     private CartItemEntity createTestCartItem() {
         ProductEntity testProduct = new ProductEntity();
-        testProduct.setId(1L);
+        testProduct.setId(PRODUCT_ID);
         OrderEntity testOrder = new OrderEntity();
-        testOrder.setId(2L);
+        testOrder.setId(ORDER_ID);
         UserEntity testUser = new UserEntity();
-        testUser.setId(3L);
+        testUser.setId(USER_ID);
 
         return new CartItemEntity()
                 .setProduct(testProduct)
