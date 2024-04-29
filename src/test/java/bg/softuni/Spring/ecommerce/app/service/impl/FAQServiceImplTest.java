a1ecbbb5302fbc51b7773d48105430b9fe0244d4
Stavromir <s.kasapov@protonmail.com>
2024-04-29 12:47:07 +0300

    update integration test

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/FAQServiceImplTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/FAQServiceImplTest.java
@@ -1,12 +1,14 @@
 package bg.softuni.Spring.ecommerce.app.service.impl;
 
 
+import bg.softuni.Spring.ecommerce.app.model.dto.CategoryDto;
 import bg.softuni.Spring.ecommerce.app.model.dto.FAQDto;
 import bg.softuni.Spring.ecommerce.app.model.dto.ProductDto;
 import bg.softuni.Spring.ecommerce.app.model.entity.FAQEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
 import bg.softuni.Spring.ecommerce.app.repository.FAQRepository;
 import bg.softuni.Spring.ecommerce.app.repository.ProductRepository;
+import bg.softuni.Spring.ecommerce.app.service.CategoryService;
 import bg.softuni.Spring.ecommerce.app.service.FAQService;
 import bg.softuni.Spring.ecommerce.app.service.ProductService;
 import org.junit.jupiter.api.AfterEach;
@@ -31,7 +33,7 @@ class FAQServiceImplTest {
     private ProductService productService;
 
     @Autowired
-    private ProductRepository productRepository;
+    private CategoryService categoryService;
 
     @BeforeEach
     void SetUp() {
@@ -46,21 +48,33 @@ class FAQServiceImplTest {
     @Test
     void testCreateFaq () throws IOException {
 
+        CategoryDto categoryDto = new CategoryDto()
+                .setName("testCategory")
+                .setDescription("testDescription");
+
+        Long categoryId = categoryService.createCategory(categoryDto).getId();
+
         ProductDto testProduct = new ProductDto()
                 .setName("Product")
                 .setPrice(1000L)
-                .setDescription("Description");
+                .setDescription("Description")
+                .setCategoryId(categoryId);
 
-        productRepository.save()
+        ProductDto productDto = productService.addProduct(testProduct);
+
+        Long id = productDto.getId();
 
         FAQDto faqDto = new FAQDto()
                 .setQuestion("q")
                 .setAnswer("a")
-                .setProductId(productId);
+                .setProductId(id);
 
         FAQEntity faq = faqService.createFAQ(faqDto);
 
         Assertions.assertNotNull(faq);
+        Assertions.assertEquals(faqDto.getAnswer(), faq.getAnswer());
+        Assertions.assertEquals(faqDto.getQuestion(), faq.getQuestion());
+        Assertions.assertEquals(faqDto.getProductId(), faq.getProduct().getId());
 
     }
 
