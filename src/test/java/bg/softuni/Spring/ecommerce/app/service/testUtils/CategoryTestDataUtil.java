0540faa24cfb00c3501998b92538409ac3e1ce10
Stavromir <s.kasapov@protonmail.com>
2024-04-29 17:49:34 +0300

    create category test util

---
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/CategoryTestDataUtil.java
@@ -0,0 +1,22 @@
+package bg.softuni.Spring.ecommerce.app.service.testUtils;
+
+import bg.softuni.Spring.ecommerce.app.model.entity.CategoryEntity;
+import bg.softuni.Spring.ecommerce.app.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryTestDataUtil {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
+    public CategoryEntity createCategory () {
+        CategoryEntity category = new CategoryEntity()
+                .setName("category")
+                .setDescription("categoryDescription");
+
+        return categoryRepository.save(category);
+    }
+}
