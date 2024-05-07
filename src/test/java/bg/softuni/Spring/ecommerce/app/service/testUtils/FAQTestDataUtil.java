f408ad7e90d721788fdb03c5cd1c2828146dd7ed
Stavromir <s.kasapov@protonmail.com>
2024-05-07 13:47:37 +0300

    add new FAQ data util

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/FAQTestDataUtil.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/FAQTestDataUtil.java
@@ -1,2 +1,48 @@
-package bg.softuni.Spring.ecommerce.app.service.testUtils;public class FAQTestDataUtil {
+package bg.softuni.Spring.ecommerce.app.service.testUtils;
+
+import bg.softuni.Spring.ecommerce.app.model.dto.FAQDto;
+import bg.softuni.Spring.ecommerce.app.model.entity.FAQEntity;
+import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
+import bg.softuni.Spring.ecommerce.app.repository.FAQRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FAQTestDataUtil {
+
+    public static final String FAQ_QUESTION = "testFaqQuestion";
+    public static final String FAQ_ANSWER = "testFaqAnswer";
+
+    @Autowired
+    private FAQRepository faqRepository;
+
+    @Autowired
+    private ProductTestDataUtil productTestDataUtil;
+
+    public FAQEntity createFaqEntity() {
+
+        ProductEntity testProduct = productTestDataUtil.createProduct();
+
+        FAQEntity testFAQ = new FAQEntity()
+                .setQuestion(FAQ_QUESTION)
+                .setAnswer(FAQ_ANSWER)
+                .setProduct(testProduct);
+
+        return faqRepository.save(testFAQ);
+    }
+
+    public FAQDto createFaqDto() {
+
+        ProductEntity testProduct = productTestDataUtil.createProduct();
+
+        return new FAQDto()
+                .setQuestion(FAQ_QUESTION)
+                .setAnswer(FAQ_ANSWER)
+                .setProductId(testProduct.getId());
+    }
+
+    public void clearAllTestData() {
+        faqRepository.deleteAll();
+        productTestDataUtil.clearAllTestData();
+    }
 }
