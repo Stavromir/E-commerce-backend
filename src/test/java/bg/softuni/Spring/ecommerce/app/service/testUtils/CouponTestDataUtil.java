6bd35749747ffcd4f6a6740670ce8b63c9ec27ca
Stavromir <s.kasapov@protonmail.com>
2024-05-07 16:27:11 +0300

    add coupon test data util

---
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/CouponTestDataUtil.java
@@ -0,0 +1,45 @@
+package bg.softuni.Spring.ecommerce.app.service.testUtils;
+
+import bg.softuni.Spring.ecommerce.app.model.entity.CouponEntity;
+import bg.softuni.Spring.ecommerce.app.repository.CouponRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class CouponTestDataUtil {
+
+    public static final String COUPON_NAME = "couponName";
+    public static final String COUPON_CODE = "couponCode";
+    public static final Long COUPON_DISCOUNT = 10L;
+    public static final Date COUPON_EXPIRATION_PAST_DATE = new Date(System.currentTimeMillis() - 86400000);
+    public static final Date COUPON_EXPIRATION_FEATURE_DATE = new Date(System.currentTimeMillis() + 86400000);
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    public CouponEntity createValidCouponEntity() {
+        CouponEntity testCoupon = new CouponEntity()
+                .setCode(COUPON_CODE)
+                .setDiscount(COUPON_DISCOUNT)
+                .setName(COUPON_NAME)
+                .setExpirationDate(COUPON_EXPIRATION_FEATURE_DATE);
+
+        return couponRepository.save(testCoupon);
+    }
+
+    public CouponEntity createExpiredCouponEntity() {
+        CouponEntity testCoupon = new CouponEntity()
+                .setCode(COUPON_CODE)
+                .setDiscount(COUPON_DISCOUNT)
+                .setName(COUPON_NAME)
+                .setExpirationDate(COUPON_EXPIRATION_PAST_DATE);
+
+        return couponRepository.save(testCoupon);
+    }
+
+    public void clearAllTestData() {
+        couponRepository.deleteAll();
+    }
+}
