76f8f6007932085cd1143260ec90f1c2b02d04e1
Stavromir <s.kasapov@protonmail.com>
2024-05-07 16:28:40 +0300

    update order service integration test

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/OrderServiceIntegrationTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/OrderServiceIntegrationTest.java
@@ -1,12 +1,16 @@
 package bg.softuni.Spring.ecommerce.app.service.impl;
 
 import bg.softuni.Spring.ecommerce.app.model.dto.AddProductInCartDto;
+import bg.softuni.Spring.ecommerce.app.model.dto.OrderDto;
+import bg.softuni.Spring.ecommerce.app.model.entity.CouponEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.OrderEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.UserEntity;
 import bg.softuni.Spring.ecommerce.app.model.enums.OrderStatusEnum;
 import bg.softuni.Spring.ecommerce.app.repository.OrderRepository;
 import bg.softuni.Spring.ecommerce.app.service.OrderService;
+import bg.softuni.Spring.ecommerce.app.service.testUtils.CouponTestDataUtil;
+import bg.softuni.Spring.ecommerce.app.service.testUtils.OrderTestDataUtil;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.ProductTestDataUtil;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.UserTestDataUtil;
 import org.junit.jupiter.api.AfterEach;
@@ -20,50 +24,74 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OrderServiceIntegrationTest {
 
-    public static final String USER_EMAIL = "user@email.com";
-
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
     private OrderService orderService;
     @Autowired
-    private UserTestDataUtil userTestDataUtil;
-    @Autowired
     private ProductTestDataUtil productTestDataUtil;
+    @Autowired
+    private OrderTestDataUtil orderTestDataUtil;
+    @Autowired
+    private CouponTestDataUtil couponTestDataUtil;
 
     @BeforeEach
     void setUp() {
-        orderRepository.deleteAll();
-        userTestDataUtil.clearAllTestData();
         productTestDataUtil.clearAllTestData();
+        orderTestDataUtil.clearAllTestData();
+        couponTestDataUtil.clearAllTestData();
     }
 
     @AfterEach
     void tearDown() {
-        orderRepository.deleteAll();
-        userTestDataUtil.clearAllTestData();
         productTestDataUtil.clearAllTestData();
+        orderTestDataUtil.clearAllTestData();
+        couponTestDataUtil.clearAllTestData();
     }
 
 
     @Test
     void testAddCartItemToOrder() {
-        UserEntity testUser = userTestDataUtil.createTestUser(USER_EMAIL);
+
+        OrderEntity emptyOrder = orderTestDataUtil.createEmptyOrder();
+        Long testUserId = emptyOrder.getUser().getId();
+
         ProductEntity testProduct = productTestDataUtil.createProduct();
 
         AddProductInCartDto addProductInCartDto = new AddProductInCartDto()
-                .setUserId(testUser.getId())
+                .setUserId(testUserId)
                 .setProductId(testProduct.getId());
-
-        orderService.createEmptyOrder(testUser);
 
         orderService.addCartItemToActiveOrder(addProductInCartDto);
 
-        OrderEntity activeOrder = orderRepository.findByUserIdAndOrderStatus(testUser.getId(),
+        OrderEntity activeOrder = orderRepository.findByUserIdAndOrderStatus(testUserId,
                 OrderStatusEnum.PENDING).get();
 
         Assertions.assertEquals(testProduct.getPrice(), activeOrder.getAmount());
         Assertions.assertEquals(testProduct.getPrice(), activeOrder.getTotalAmount());
         Assertions.assertEquals(1, activeOrder.getCartItems().size());
+    }
+
+    @Test
+    void testApplyCoupon() {
+        CouponEntity testCoupon = couponTestDataUtil.createValidCouponEntity();
+        OrderEntity filledOrder = orderTestDataUtil.createFilledOrder();
+
+        Long amountAfterCouponApply = 900L;
+        Long discountAfterCouponApply = 100L;
+
+        Long testUserId = filledOrder.getUser().getId();
+        String couponCode = testCoupon.getCode();
+
+        orderService.applyCoupon(testUserId, couponCode);
+
+        OrderEntity orderAfterCouponApply = orderRepository.findById(filledOrder.getId()).get();
+
+        Assertions.assertNotNull(orderAfterCouponApply.getCoupon());
+        Assertions.assertEquals(testCoupon.getCode(), orderAfterCouponApply.getCoupon().getCode());
+        Assertions.assertEquals(testCoupon.getDiscount(), orderAfterCouponApply.getCoupon().getDiscount());
+        Assertions.assertEquals(amountAfterCouponApply, orderAfterCouponApply.getAmount());
+        Assertions.assertEquals(filledOrder.getTotalAmount(), orderAfterCouponApply.getTotalAmount());
+        Assertions.assertEquals(discountAfterCouponApply, orderAfterCouponApply.getDiscount());
     }
 }
