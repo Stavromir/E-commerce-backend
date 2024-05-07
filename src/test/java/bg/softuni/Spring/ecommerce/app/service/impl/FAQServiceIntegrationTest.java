38eb8bf7f25294d2ced61185490a118c95b786bd
Stavromir <s.kasapov@protonmail.com>
2024-05-07 13:48:25 +0300

    update integration test

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/FAQServiceIntegrationTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/FAQServiceIntegrationTest.java
@@ -1,18 +1,9 @@
 package bg.softuni.Spring.ecommerce.app.service.impl;
 
-
-import bg.softuni.Spring.ecommerce.app.model.dto.CategoryDto;
 import bg.softuni.Spring.ecommerce.app.model.dto.FAQDto;
-import bg.softuni.Spring.ecommerce.app.model.dto.ProductDto;
-import bg.softuni.Spring.ecommerce.app.model.entity.CategoryEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.FAQEntity;
-import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
-import bg.softuni.Spring.ecommerce.app.repository.FAQRepository;
-import bg.softuni.Spring.ecommerce.app.service.CategoryService;
 import bg.softuni.Spring.ecommerce.app.service.FAQService;
-import bg.softuni.Spring.ecommerce.app.service.ProductService;
-import bg.softuni.Spring.ecommerce.app.service.testUtils.CategoryTestDataUtil;
-import bg.softuni.Spring.ecommerce.app.service.testUtils.ProductTestDataUtil;
+import bg.softuni.Spring.ecommerce.app.service.testUtils.FAQTestDataUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,38 +16,26 @@ import java.io.IOException;
 @SpringBootTest
 class FAQServiceIntegrationTest {
 
-    public static final String FIRST_FAQ_QUESTION = "firstFaqQuestion";
-    public static final String FIRST_FAQ_ANSWER = "firstFaqAnswer";
-
-
     @Autowired
     private FAQService faqService;
 
     @Autowired
-    private FAQRepository faqRepository;
-
-    @Autowired
-    private ProductTestDataUtil productTestDataUtil;
+    private FAQTestDataUtil faqTestDataUtil;
 
     @BeforeEach
     void SetUp() {
-        faqRepository.deleteAll();
-        productTestDataUtil.clearAllTestData();
+        faqTestDataUtil.clearAllTestData();
     }
 
     @AfterEach
     void tearDown() {
-        faqRepository.deleteAll();
-        productTestDataUtil.clearAllTestData();
+        faqTestDataUtil.clearAllTestData();
     }
 
     @Test
-    void testCreateFaq () throws IOException {
+    void testCreateFaq() {
 
-        ProductEntity testProduct = productTestDataUtil.createProduct();
-        Long productId = testProduct.getId();
-
-        FAQDto testFAQ = getFaqDto(FIRST_FAQ_QUESTION, FIRST_FAQ_ANSWER, productId);
+        FAQDto testFAQ = faqTestDataUtil.createFaqDto();
 
         FAQEntity createdFAQ = faqService.createFAQ(testFAQ);
 
@@ -67,28 +46,17 @@ class FAQServiceIntegrationTest {
     }
 
     @Test
-    void testGetAllFaqByProductId () throws IOException {
+    void testGetAllFaqByProductId() {
 
-        ProductEntity testProduct = productTestDataUtil.createProduct();
-        Long testProductId = testProduct.getId();
-
-        FAQDto firstTestFAQ = getFaqDto(FIRST_FAQ_QUESTION, FIRST_FAQ_ANSWER, testProductId);
-
-        FAQEntity firstCreatedFAQ = faqService.createFAQ(firstTestFAQ);
+        FAQEntity testFAQ = faqTestDataUtil.createFaqEntity();
+        Long testProductId = testFAQ.getProduct().getId();
 
         FAQDto firstReturnedFAQ = faqService.getAllFaqByProductId(testProductId).get(0);
 
         Assertions.assertNotNull(firstReturnedFAQ);
-        Assertions.assertEquals(firstCreatedFAQ.getId(), firstReturnedFAQ.getId());
-        Assertions.assertEquals(firstCreatedFAQ.getQuestion(), firstReturnedFAQ.getQuestion());
-        Assertions.assertEquals(firstCreatedFAQ.getAnswer(), firstReturnedFAQ.getAnswer());
+        Assertions.assertEquals(testFAQ.getId(), firstReturnedFAQ.getId());
+        Assertions.assertEquals(testFAQ.getQuestion(), firstReturnedFAQ.getQuestion());
+        Assertions.assertEquals(testFAQ.getAnswer(), firstReturnedFAQ.getAnswer());
 
-    }
-
-    private static FAQDto getFaqDto(String question, String answer,Long productId) {
-        return new FAQDto()
-                .setQuestion(question)
-                .setAnswer(answer)
-                .setProductId(productId);
     }
 }
