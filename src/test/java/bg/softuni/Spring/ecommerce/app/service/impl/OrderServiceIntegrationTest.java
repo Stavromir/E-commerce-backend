14cb191dfca0f270069fec20a2067baede44ba38
Stavromir Kasapov <127794574+Stavromir@users.noreply.github.com>
2024-05-08 14:20:19 +0300

    add new integration test

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/OrderServiceIntegrationTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/OrderServiceIntegrationTest.java
@@ -76,7 +76,7 @@ class OrderServiceIntegrationTest {
     @Test
     void testApplyValidCoupon() {
         CouponEntity testCoupon = couponTestDataUtil.createValidCouponEntity();
-        OrderEntity filledOrder = orderTestDataUtil.createFilledOrder();
+        OrderEntity filledOrder = orderTestDataUtil.createFilledOrderInitialQuantity();
 
         Long amountAfterCouponApply = 900L;
         Long discountAfterCouponApply = 100L;
@@ -99,7 +99,7 @@ class OrderServiceIntegrationTest {
     @Test
     void testApplyExpiredCouponThrowExc() {
         CouponEntity testCoupon = couponTestDataUtil.createExpiredCouponEntity();
-        OrderEntity filledOrder = orderTestDataUtil.createFilledOrder();
+        OrderEntity filledOrder = orderTestDataUtil.createFilledOrderInitialQuantity();
 
         Long testUserId = filledOrder.getUser().getId();
         String couponCode = testCoupon.getCode();
@@ -113,7 +113,7 @@ class OrderServiceIntegrationTest {
     @Test
     void testIncreaseProductQuantityWithCoupon() {
         CouponEntity testCoupon = couponTestDataUtil.createValidCouponEntity();
-        OrderEntity testOrder = orderTestDataUtil.createFilledOrder();
+        OrderEntity testOrder = orderTestDataUtil.createFilledOrderInitialQuantity();
         testOrder.setCoupon(testCoupon);
         orderRepository.save(testOrder);
 
@@ -134,15 +134,9 @@ class OrderServiceIntegrationTest {
         Assertions.assertEquals(1800, savedOrder.getAmount());
     }
 
-    private static AddProductInCartDto getAddProductInCartDto(Long testProductId, Long testUserId) {
-        return new AddProductInCartDto()
-                .setProductId(testProductId)
-                .setUserId(testUserId);
-    }
-
     @Test
     void testIncreaseProductQuantityWithoutCoupon() {
-        OrderEntity testOrder = orderTestDataUtil.createFilledOrder();
+        OrderEntity testOrder = orderTestDataUtil.createFilledOrderInitialQuantity();
 
         Long testUserId = testOrder.getUser().getId();
         CartItemEntity testCartItem = testOrder.getCartItems().get(0);
@@ -159,5 +153,35 @@ class OrderServiceIntegrationTest {
         Assertions.assertEquals(testCartItem.getQuantity() + 1, savedCartItem.getQuantity());
         Assertions.assertEquals(testOrder.getTotalAmount() * 2, savedOrder.getTotalAmount());
         Assertions.assertEquals(testOrder.getAmount() * 2, savedOrder.getAmount());
+    }
+
+    @Test
+    void testDecreaseProductQuantityWithCoupon() {
+        CouponEntity testCoupon = couponTestDataUtil.createValidCouponEntity();
+        OrderEntity testOrder = orderTestDataUtil.createFilledOrderIncreasedQuantity();
+        testOrder.setCoupon(testCoupon);
+        orderRepository.save(testOrder);
+
+        Long testUserId = testOrder.getUser().getId();
+        CartItemEntity testCartItem = testOrder.getCartItems().get(0);
+        Long testProductId = testCartItem.getProduct().getId();
+
+        AddProductInCartDto addProductInCartDto =
+                getAddProductInCartDto(testProductId, testUserId);
+
+        orderService.decreaseProductQuantity(addProductInCartDto);
+
+        OrderEntity savedOrder = orderRepository.findById(testOrder.getId()).get();
+        CartItemEntity savedCartItem = savedOrder.getCartItems().get(0);
+
+        Assertions.assertEquals(testCartItem.getQuantity() - 1, savedCartItem.getQuantity());
+        Assertions.assertEquals(testOrder.getTotalAmount() / 2, savedOrder.getTotalAmount());
+        Assertions.assertEquals(900, savedOrder.getAmount());
+    }
+
+    private static AddProductInCartDto getAddProductInCartDto(Long testProductId, Long testUserId) {
+        return new AddProductInCartDto()
+                .setProductId(testProductId)
+                .setUserId(testUserId);
     }
 }
