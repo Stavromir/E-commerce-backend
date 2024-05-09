a441be35ec5fc8f4a5fb88d6a94509bc51aa39b6
Stavromir <s.kasapov@protonmail.com>
2024-05-09 17:15:25 +0300

    add review test data util

---
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/ReviewTestDataUtil.java
@@ -0,0 +1,40 @@
+package bg.softuni.Spring.ecommerce.app.service.testUtils;
+
+import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
+import bg.softuni.Spring.ecommerce.app.model.entity.ReviewEntity;
+import bg.softuni.Spring.ecommerce.app.model.entity.UserEntity;
+import bg.softuni.Spring.ecommerce.app.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReviewTestDataUtil {
+
+    public static final String REVIEW_DESCRIPTION = "reviewDescription";
+    public static final Long REVIEW_RATING = 5L;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private ProductTestDataUtil productTestDataUtil;
+    @Autowired
+    private UserTestDataUtil userTestDataUtil;
+
+    public ReviewEntity createReviewEntity() {
+
+        ProductEntity testProduct = productTestDataUtil.createProduct();
+        UserEntity testUser = userTestDataUtil.createTestUser();
+
+        return new ReviewEntity()
+                .setProduct(testProduct)
+                .setUser(testUser)
+                .setDescription(REVIEW_DESCRIPTION)
+                .setRating(REVIEW_RATING);
+    }
+
+
+
+    public void clearAllTestData() {
+        reviewRepository.deleteAll();
+    }
+}
