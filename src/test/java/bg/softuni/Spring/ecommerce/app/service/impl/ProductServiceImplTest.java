a3874efff7929c9c0e93ff44ad64b5183f0f880b
Stavromir <s.kasapov@protonmail.com>
2024-05-09 13:47:39 +0300

    add new integration test

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/ProductServiceImplTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/ProductServiceImplTest.java
@@ -1,11 +1,18 @@
 package bg.softuni.Spring.ecommerce.app.service.impl;
 
+import bg.softuni.Spring.ecommerce.app.model.dto.ProductDto;
+import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
 import bg.softuni.Spring.ecommerce.app.repository.ProductRepository;
 import bg.softuni.Spring.ecommerce.app.service.ProductService;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.OrderTestDataUtil;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.ProductTestDataUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +28,22 @@ class ProductServiceImplTest {
     private ProductTestDataUtil productTestDataUtil;
     @Autowired
     private OrderTestDataUtil orderTestDataUtil;
+
+
+    @Test
+    void testAddProduct() throws IOException {
+        ProductDto testProductDto = productTestDataUtil.createProductDto();
+
+        ProductDto returnedProductDto = productService.addProduct(testProductDto);
+        Long productId = returnedProductDto.getId();
+
+        ProductEntity product = productRepository.findById(productId).get();
+
+        Assertions.assertNotNull(product);
+        Assertions.assertEquals(testProductDto.getName(), product.getName());
+        Assertions.assertEquals(testProductDto.getDescription(), product.getDescription());
+        Assertions.assertEquals(testProductDto.getPrice(), product.getPrice());
+        Assertions.assertEquals(testProductDto.getCategoryId(), product.getCategory().getId());
+    }
 
 }
