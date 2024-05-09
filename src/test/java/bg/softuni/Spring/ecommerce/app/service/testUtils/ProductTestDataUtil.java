e69999875aae95f0ec352b44848636f9d291a2cc
Stavromir <s.kasapov@protonmail.com>
2024-05-09 13:46:54 +0300

    add new methods

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/ProductTestDataUtil.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/ProductTestDataUtil.java
@@ -1,11 +1,13 @@
 package bg.softuni.Spring.ecommerce.app.service.testUtils;
 
 import bg.softuni.Spring.ecommerce.app.model.dto.AddProductInCartDto;
+import bg.softuni.Spring.ecommerce.app.model.dto.ProductDto;
 import bg.softuni.Spring.ecommerce.app.model.entity.CategoryEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
 import bg.softuni.Spring.ecommerce.app.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HexFormat;
 
@@ -36,6 +38,16 @@ public class ProductTestDataUtil {
                 .setCategory(category);
 
         return productRepository.save(product);
+    }
+
+    public ProductDto createProductDto() {
+        CategoryEntity category = categoryTestDataUtil.createCategory();
+
+        return new ProductDto()
+                .setName(PRODUCT_NAME)
+                .setDescription(PRODUCT_DESCRIPTION)
+                .setPrice(PRODUCT_PRICE)
+                .setCategoryId(category.getId());
     }
 
     public void clearAllTestData() {
