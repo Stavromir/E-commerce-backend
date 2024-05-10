89cb8ddd39c60d981acfa84c11302ce3cc4ea439
Stavromir <s.kasapov@protonmail.com>
2024-05-10 16:06:10 +0300

    add integration tests

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/UserServiceImplTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/UserServiceImplTest.java
@@ -4,7 +4,10 @@ import bg.softuni.Spring.ecommerce.app.model.dto.SignupRequestDto;
 import bg.softuni.Spring.ecommerce.app.model.entity.UserEntity;
 import bg.softuni.Spring.ecommerce.app.repository.UserRepository;
 import bg.softuni.Spring.ecommerce.app.service.UserService;
+import bg.softuni.Spring.ecommerce.app.service.exception.ObjectNotFoundException;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.UserTestDataUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,6 +27,16 @@ class UserServiceImplTest {
     @Autowired
     private UserTestDataUtil userTestDataUtil;
 
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
     @Test
     void testCreateUser() {
         SignupRequestDto testSignupRequestDto = userTestDataUtil
@@ -39,5 +52,32 @@ class UserServiceImplTest {
         assertEquals(testSignupRequestDto.getName(), userEntity.getName());
         assertTrue(Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8()
                 .matches(testSignupRequestDto.getPassword(),userEntity.getPassword()));
+    }
+
+    @Test
+    void testHasUserWithEmail() {
+        UserEntity testUser = userTestDataUtil.createTestUser();
+
+        boolean hasUserWithEmail = userService.hasUserWithEmail(testUser.getEmail());
+        assertTrue(hasUserWithEmail);
+    }
+
+    @Test
+    void testGetUserById() {
+        UserEntity testUser = userTestDataUtil.createTestUser();
+
+        UserEntity returnedUserEntity = userService.getUserById(testUser.getId());
+
+        assertNotNull(returnedUserEntity);
+        assertEquals(testUser.getName(), returnedUserEntity.getName());
+        assertEquals(testUser.getEmail(), returnedUserEntity.getEmail());
+    }
+
+    @Test
+    void testGetUserByIdThrowExc() {
+        assertThrows(
+                ObjectNotFoundException.class,
+                () -> userService.getUserById(101L)
+        );
     }
 }
