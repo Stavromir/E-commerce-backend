e723019b26b8e2638024853548a4a69ba0af6fc2
Stavromir <s.kasapov@protonmail.com>
2024-05-10 17:59:40 +0300

    update test data utils

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/WishListTestDataUtil.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/WishListTestDataUtil.java
@@ -3,15 +3,32 @@ package bg.softuni.Spring.ecommerce.app.service.testUtils;
 import bg.softuni.Spring.ecommerce.app.model.dto.WishListDto;
 import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
 import bg.softuni.Spring.ecommerce.app.model.entity.UserEntity;
+import bg.softuni.Spring.ecommerce.app.model.entity.WishListEntity;
+import bg.softuni.Spring.ecommerce.app.repository.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WishListTestDataUtil {
+
+
+    @Autowired
+    private WishListRepository wishListRepository;
     @Autowired
     private ProductTestDataUtil productTestDataUtil;
     @Autowired
     private UserTestDataUtil userTestDataUtil;
+
+    public WishListEntity createWishListEntity() {
+        UserEntity testUserEntity = userTestDataUtil.createTestUser();
+        ProductEntity testProductEntity = productTestDataUtil.createProduct();
+
+        WishListEntity wishListEntity = new WishListEntity()
+                .setProduct(testProductEntity)
+                .setUser(testUserEntity);
+
+        return wishListRepository.save(wishListEntity);
+    }
 
     public WishListDto createWishListDto() {
         UserEntity testUserEntity = userTestDataUtil.createTestUser();
@@ -20,5 +37,11 @@ public class WishListTestDataUtil {
         return new WishListDto()
                 .setUserId(testUserEntity.getId())
                 .setProductId(testDataUtilProduct.getId());
+    }
+
+    public void clearAllTestData() {
+        wishListRepository.deleteAll();
+        productTestDataUtil.clearAllTestData();
+        userTestDataUtil.clearAllTestData();
     }
 }
