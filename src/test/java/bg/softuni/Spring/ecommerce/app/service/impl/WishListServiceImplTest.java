5d85ee2962a0ad92c2eba50914c946e2f6e46bc8
Stavromir <s.kasapov@protonmail.com>
2024-05-10 18:00:15 +0300

    add integration tests

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/WishListServiceImplTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/WishListServiceImplTest.java
@@ -1,14 +1,18 @@
 package bg.softuni.Spring.ecommerce.app.service.impl;
 
 import bg.softuni.Spring.ecommerce.app.model.dto.WishListDto;
+import bg.softuni.Spring.ecommerce.app.model.entity.WishListEntity;
 import bg.softuni.Spring.ecommerce.app.repository.WishListRepository;
 import bg.softuni.Spring.ecommerce.app.service.WishListService;
-import bg.softuni.Spring.ecommerce.app.service.testUtils.ProductTestDataUtil;
-import bg.softuni.Spring.ecommerce.app.service.testUtils.UserTestDataUtil;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.WishListTestDataUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,11 +26,58 @@ class WishListServiceImplTest {
     @Autowired
     private WishListTestDataUtil wishListTestDataUtil;
 
+    @BeforeEach
+    void setUp() {
+        wishListTestDataUtil.clearAllTestData();
+    }
+
+    @AfterEach
+    void tearDown() {
+        wishListTestDataUtil.clearAllTestData();
+    }
+
 
     @Test
     void testAddProductToWishList() {
         WishListDto testWishListDto = wishListTestDataUtil.createWishListDto();
 
+        Long wishListEntityId = wishListService.addProductToWishList(testWishListDto);
+
+        Optional<WishListEntity> testWishListEntity = wishListRepository
+                .findById(wishListEntityId);
+
+        assertTrue(testWishListEntity.isPresent());
+        WishListEntity wishListEntity = testWishListEntity.get();
+        assertEquals(testWishListDto.getProductId(), wishListEntity.getProduct().getId());
+        assertEquals(testWishListDto.getUserId(), wishListEntity.getUser().getId());
+    }
+
+    @Test
+    void testAddProductToWishListThrowExc() {
+        wishListTestDataUtil.createWishListEntity();
+        WishListDto testWishListDto = wishListTestDataUtil.createWishListDto();
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> wishListService.addProductToWishList(testWishListDto)
+        );
+    }
+
+    @Test
+    void testGetAllProductsInWishList() {
+        WishListEntity wishListEntity = wishListTestDataUtil.createWishListEntity();
+
+        List<WishListDto> allProductsInUserWishList = wishListService
+                .getAllProductsInUserWishList(wishListEntity.getUser().getId());
+
+        WishListDto wishListDto = allProductsInUserWishList.get(0);
+
+        assertNotNull(wishListDto);
+        assertEquals(wishListEntity.getId(), wishListDto.getId());
+        assertEquals(wishListEntity.getProduct().getPrice(), wishListDto.getPrice());
+        assertEquals(wishListEntity.getProduct().getDescription(), wishListDto.getProductDescription());
+        assertEquals(wishListEntity.getProduct().getName(), wishListDto.getProductName());
+        assertArrayEquals(wishListEntity.getProduct().getImg(), wishListDto.getReturnedImg());
     }
 
 }
