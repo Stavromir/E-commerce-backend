333c4ce21200fde20197c29dea00d2eb17914739
Stavromir <s.kasapov@protonmail.com>
2024-05-10 17:00:55 +0300

    add wishList test data utils

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/WishListTestDataUtil.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/WishListTestDataUtil.java
@@ -1,2 +1,24 @@
-package bg.softuni.Spring.ecommerce.app.service.testUtils;public class WishListTestDataUtil {
+package bg.softuni.Spring.ecommerce.app.service.testUtils;
+
+import bg.softuni.Spring.ecommerce.app.model.dto.WishListDto;
+import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
+import bg.softuni.Spring.ecommerce.app.model.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WishListTestDataUtil {
+    @Autowired
+    private ProductTestDataUtil productTestDataUtil;
+    @Autowired
+    private UserTestDataUtil userTestDataUtil;
+
+    public WishListDto createWishListDto() {
+        UserEntity testUserEntity = userTestDataUtil.createTestUser();
+        ProductEntity testDataUtilProduct = productTestDataUtil.createProduct();
+
+        return new WishListDto()
+                .setUserId(testUserEntity.getId())
+                .setProductId(testDataUtilProduct.getId());
+    }
 }
