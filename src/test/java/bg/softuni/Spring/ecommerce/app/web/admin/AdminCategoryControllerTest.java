5592be6304ba01a7d21291f5ee853c9d90304d49
Stavromir <s.kasapov@protonmail.com>
2024-05-13 10:16:17 +0300

    initial commit integration test

---
+++ src/test/java/bg/softuni/Spring/ecommerce/app/web/admin/AdminCategoryControllerTest.java
@@ -0,0 +1,12 @@
+package bg.softuni.Spring.ecommerce.app.web.admin;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdminCategoryControllerTest {
+
+}
