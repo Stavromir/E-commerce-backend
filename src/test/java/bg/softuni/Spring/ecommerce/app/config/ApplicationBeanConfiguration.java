f7dd83405d708f25f9a1f201f85acaad9b6045e3
Stavromir <s.kasapov@protonmail.com>
2024-05-14 14:54:24 +0300

    create Application Bean Config, add @Bean Gson

--- src/test/java/bg/softuni/Spring/ecommerce/app/config/ApplicationBeanConfiguration.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/config/ApplicationBeanConfiguration.java
@@ -1,2 +1,20 @@
-package bg.softuni.Spring.ecommerce.app.config;public class ApplicationBeanConfiguration {
+package bg.softuni.Spring.ecommerce.app.config;
+
+import bg.softuni.Spring.ecommerce.app.service.testUtils.LocalDateTimeTypeAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+                .create();
+    }
 }
