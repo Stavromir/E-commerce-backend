60c93523f7b7cc6b7d00d3c8dc3c68f9704160a4
Stavromir <s.kasapov@protonmail.com>
2024-05-14 15:21:01 +0300

    add @BeforeEach and @AfterEach

--- src/test/java/bg/softuni/Spring/ecommerce/app/web/admin/AdminCategoryControllerTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/web/admin/AdminCategoryControllerTest.java
@@ -4,32 +4,25 @@ import bg.softuni.Spring.ecommerce.app.model.dto.CategoryDto;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.CategoryTestDataUtil;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.JwtTestDataUtil;
 import com.google.gson.Gson;
-import org.apache.catalina.security.SecurityConfig;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class AdminCategoryControllerTest {
 
-
+    @Autowired
+    private MockMvc mockMvc;
     @Autowired
     private CategoryTestDataUtil categoryTestDataUtil;
     @Autowired
@@ -37,19 +30,15 @@ class AdminCategoryControllerTest {
     @Autowired
     private JwtTestDataUtil jwtTestDataUtil;
 
-//    @Autowired
-//    private WebApplicationContext context;
+    @BeforeEach
+    void setUp() {
+        categoryTestDataUtil.cleanAllTestData();
+    }
 
-    @Autowired
-    private MockMvc mockMvc;
-
-//    @BeforeEach
-//    void setUp() {
-//        mockMvc = MockMvcBuilders
-//                .webAppContextSetup(context)
-//                .apply(springSecurity())
-//                .build();
-//    }
+    @AfterEach
+    void tearDown() {
+        categoryTestDataUtil.cleanAllTestData();
+    }
 
     @Test
     void testCreateCategory() throws Exception {
