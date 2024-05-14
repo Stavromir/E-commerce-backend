9f8d9f6c199df1e1d1bd7522ed5b21456f6b94d8
Stavromir <s.kasapov@protonmail.com>
2024-05-14 15:06:25 +0300

    change Date with LocalDateTime

--- src/main/java/bg/softuni/Spring/ecommerce/app/utils/impl/DateTimeImpl.java
+++ src/main/java/bg/softuni/Spring/ecommerce/app/utils/impl/DateTimeImpl.java
@@ -3,12 +3,13 @@ package bg.softuni.Spring.ecommerce.app.utils.impl;
 import bg.softuni.Spring.ecommerce.app.utils.DateTime;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Component
 public class DateTimeImpl implements DateTime {
     @Override
-    public Date getDate() {
-        return new Date();
+    public LocalDateTime getDate() {
+        return LocalDateTime.now();
     }
 }
