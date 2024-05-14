907d3c407ea93c22eba3b82d33e1cf6c66b0f5b1
Stavromir <s.kasapov@protonmail.com>
2024-05-14 15:02:46 +0300

    code refactor

--- src/test/java/bg/softuni/Spring/ecommerce/app/web/admin/AdminCouponControllerTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/web/admin/AdminCouponControllerTest.java
@@ -1,30 +1,51 @@
 package bg.softuni.Spring.ecommerce.app.web.admin;
 
+import bg.softuni.Spring.ecommerce.app.model.dto.CouponDto;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.CouponTestDataUtil;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.JwtTestDataUtil;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class AdminCouponControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
-    @Autowired
     private CouponTestDataUtil couponTestDataUtil;
     @Autowired
     private JwtTestDataUtil jwtTestDataUtil;
-
+    @Autowired
+    private Gson gson;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void testCreateCoupon() {
+    void testCreateCoupon() throws Exception {
 
+        String jwtToken = jwtTestDataUtil.getJwtToken(mockMvc);
+        CouponDto couponDto = couponTestDataUtil.createCouponDto();
+
+        String content = gson.toJson(couponDto);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/admin/coupons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8")
+                        .header(HttpHeaders.AUTHORIZATION, jwtToken)
+                        .content(content)
+        )
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isNumber());
     }
 
     @Test
