9edccf011dea7e333068cffa6d37976bcfc09466
Stavromir <s.kasapov@protonmail.com>
2024-05-15 15:43:13 +0300

    code refactor

--- src/test/java/bg/softuni/Spring/ecommerce/app/web/admin/AdminProductControllerTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/web/admin/AdminProductControllerTest.java
@@ -16,8 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 @AutoConfigureMockMvc
 class AdminProductControllerTest {
