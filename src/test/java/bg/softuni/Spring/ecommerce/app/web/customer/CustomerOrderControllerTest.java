40e3f362e5313d6d0b5d402b0c33701b92954673
Stavromir <s.kasapov@protonmail.com>
2024-05-16 12:48:18 +0300

    add new integration test

--- src/test/java/bg/softuni/Spring/ecommerce/app/web/customer/CustomerOrderControllerTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/web/customer/CustomerOrderControllerTest.java
@@ -1,24 +1,40 @@
 package bg.softuni.Spring.ecommerce.app.web.customer;
 
+import bg.softuni.Spring.ecommerce.app.model.dto.AddProductInCartDto;
+import bg.softuni.Spring.ecommerce.app.model.entity.OrderEntity;
+import bg.softuni.Spring.ecommerce.app.model.entity.ProductEntity;
+import bg.softuni.Spring.ecommerce.app.service.testUtils.JwtTestDataUtil;
 import bg.softuni.Spring.ecommerce.app.service.testUtils.OrderTestDataUtil;
+import bg.softuni.Spring.ecommerce.app.service.testUtils.ProductTestDataUtil;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class CustomerOrderControllerTest {
 
+    private static final String BASE_URL = "/api/customer";
+
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private JwtTestDataUtil jwtTestDataUtil;
     @Autowired
     private Gson gson;
     @Autowired
     private OrderTestDataUtil orderTestDataUtil;
+    @Autowired
+    private ProductTestDataUtil productTestDataUtil;
 
     @BeforeEach
     void setUp() {
@@ -28,6 +44,35 @@ class CustomerOrderControllerTest {
     @AfterEach
     void tearDown() {
         orderTestDataUtil.clearAllTestData();
+    }
+
+    @Test
+    void testAddProductToCart() throws Exception {
+        OrderEntity emptyOrder = orderTestDataUtil.createEmptyOrder();
+        Long testUserId = emptyOrder.getUser().getId();
+
+        ProductEntity testProduct = productTestDataUtil.createProduct();
+
+        AddProductInCartDto addProductInCartDto = new AddProductInCartDto()
+                .setUserId(testUserId)
+                .setProductId(testProduct.getId());
+
+        String jwtToken = getJwtToken();
+        String content = gson.toJson(addProductInCartDto);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(BASE_URL + "/carts")
+                        .characterEncoding("utf-8")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, jwtToken)
+                        .content(content)
+        )
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isNumber());
+    }
+
+    private String getJwtToken() throws Exception {
+        return jwtTestDataUtil.getJwtToken(mockMvc);
     }
 
 
