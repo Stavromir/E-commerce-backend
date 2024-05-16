f25b57145d29378b5976742c03101ca02b0c8882
Stavromir <s.kasapov@protonmail.com>
2024-05-16 15:32:18 +0300

    add testGetProducts()

--- src/test/java/bg/softuni/Spring/ecommerce/app/web/customer/CustomerProductControllerTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/web/customer/CustomerProductControllerTest.java
@@ -5,14 +5,20 @@ import bg.softuni.Spring.ecommerce.app.service.testUtils.ProductTestDataUtil;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class CustomerProductControllerTest {
+
+    private static final String BASE_URL = "/api/customer";
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,6 +37,24 @@ class CustomerProductControllerTest {
     @AfterEach
     void tearDown() {
         productTestDataUtil.clearAllTestData();
+    }
+
+    @Test
+    void testGetProducts() throws Exception {
+        productTestDataUtil.createProduct();
+        String jwtToken = getJwtToken();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(BASE_URL + "/products")
+                        .characterEncoding("utf-8")
+                        .header(HttpHeaders.AUTHORIZATION, jwtToken)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
+    }
+
+    private String getJwtToken() throws Exception {
+        return jwtTestDataUtil.getJwtToken(mockMvc);
     }
 
 }
