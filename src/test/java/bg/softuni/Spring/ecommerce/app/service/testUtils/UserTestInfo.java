a2d8c242b302977d4d173397b2ece0778f274028
Stavromir <s.kasapov@protonmail.com>
2024-05-17 17:44:58 +0300

    create User Info class

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/UserTestInfo.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/testUtils/UserTestInfo.java
@@ -1,2 +1,11 @@
-package bg.softuni.Spring.ecommerce.app.service.testUtils;public class UserEmails {
+package bg.softuni.Spring.ecommerce.app.service.testUtils;
+
+public class UserTestInfo {
+    public static final String USER_EMAIL_1 = "user1@email.com";
+    public static final String USER_EMAIL_2 = "user2@email.com";
+    public static final String USER_EMAIL_3 = "user3@email.com";
+
+    public static final String IMG_HEX = "e04f";
+    public static final String USERNAME = "testUser";
+    public static final String USER_PASSWORD = "userTestPass";
 }
