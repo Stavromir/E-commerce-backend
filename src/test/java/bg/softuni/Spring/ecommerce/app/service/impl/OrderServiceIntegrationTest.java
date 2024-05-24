90e10670802d66d739b59644c8e3947c24086f8e
Stavromir Kasapov <127794574+Stavromir@users.noreply.github.com>
2024-05-24 10:51:03 +0300

    code refactor

--- src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/OrderServiceIntegrationTest.java
+++ src/test/java/bg/softuni/Spring/ecommerce/app/service/impl/OrderServiceIntegrationTest.java
@@ -197,7 +197,9 @@ class OrderServiceIntegrationTest {
 
         orderService.decreaseProductQuantity(addProductInCartDto);
 
-        OrderEntity savedOrder = orderRepository.findById(testOrder.getId()).get();
+        Optional<OrderEntity> optionalOrderById = orderRepository.findById(testOrder.getId());
+        Assertions.assertTrue(optionalOrderById.isPresent());
+        OrderEntity savedOrder = optionalOrderById.get();
         CartItemEntity savedCartItem = savedOrder.getCartItems().get(0);
 
         Assertions.assertEquals(testCartItem.getQuantity() - 1, savedCartItem.getQuantity());
